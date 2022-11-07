65dc42a34ca0d18827b4f7fb3a86f9c6952a6a74
romariou76 <illeniumr@gmail.com>
2022-11-07 17:49:39 -0500

    feat:Diseño del componente Collaborators realizado

--- client/src/components/Collaborator.js
+++ client/src/components/Collaborator.js
@@ -1,9 +1,14 @@
 function Collaborator(props) {
   return(
   <div className="collaborator-item">
-    <h4>{props.name}</h4>
-    <span className="foto-collaborator"></span>
-    <p>{props.cargo}</p>
+    <img className="foto-collaborator" src="https://placeimg.com/192/192/people"></img>
+    <p className="name-collaborator">{props.name}</p>
+    <p className="cargo-collaborator">{props.cargo}</p>
+    <div className="collaborator-options">
+      <p>Assign</p>
+      <p>View</p>
+    </div>
+    <button>VIEW DETAILS</button>
   </div>
   )
 }
--- client/src/components/Collaborators.js
+++ client/src/components/Collaborators.js
@@ -1,47 +1,68 @@
 import "../styles/Collaborators.css"
 import Collaborator from "./Collaborator";
+import UI from "../images/ui.png"
 
 function Collaborators() {
-    return ( 
-        <div className="collaborators">
-            <div className="collaborator-navbar">
-                <p>Collaborators</p>
-            </div>
-            <div className="collaborator-content">
-                <div className="colaborator-items">
-                    <Collaborator
-                        name="Fernando"
-                        foto="foto"
-                        cargo="Frontend"
-                    />
-                    <Collaborator
-                        name="Paola"
-                        foto="foto"
-                        cargo="Backend"
-                    />
-                    <Collaborator
-                        name="Diego"
-                        foto="foto"
-                        cargo="Diseñador UI"
-                    />
-                    <Collaborator
-                        name="Sofia"
-                        foto="foto"
-                        cargo="Diseñador UX"
-                    />
-                    <Collaborator
-                        name="Sofia"
-                        foto="foto"
-                        cargo="Analista"
-                    />
-                </div>
-                <div className="collaborator-process">
-                    <div></div>
-                    
-                </div>
-            </div>
+    return (
+      <div className="collaborators">
+        <div className="collaborator-navbar">
+          <p>Team Members</p>
+          <div class="group">
+            <input placeholder="Search" type="search" className="input" />
+            <svg class="icon" aria-hidden="true" viewBox="0 0 24 24"><g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g></svg>
+          </div>
         </div>
-     );
+
+        <div className="collaborator-container">
+          <div>
+            <p className="overview-p">Overview</p>
+          </div>
+          <div className="collaborator-content">
+
+{/* --------------Componente collaborator------------ */}
+            <div className="roles-content">
+              <img alt="UI" src={UI} className="rol-logo"></img>
+              <div className="rol-info">
+                <div className="rol-info1">5</div>
+                <div className="rol-info2">Node.js Developers</div>
+              </div>
+            </div>
+            <div className="roles-content">
+              <img alt="UI" src={UI} className="rol-logo"></img>
+              <div className="rol-info">
+                <div className="rol-info1">5</div>
+                <div className="rol-info2">Node.js Developers</div>
+              </div>
+            </div>
+            <div className="roles-content">
+              <img alt="UI" src={UI} className="rol-logo"></img>
+              <div className="rol-info">
+                <div className="rol-info1">5</div>
+                <div className="rol-info2">Node.js Developers</div>
+              </div>
+            </div>
+            <div className="roles-content">
+              <img alt="UI" src={UI} className="rol-logo"></img>
+              <div className="rol-info">
+                <div className="rol-info1">5</div>
+                <div className="rol-info2">Node.js Developers</div>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+
+        <div className="collaborators-card-content">
+          <p className="todays">Collaborators</p>
+          <div className="colaborator-items">
+            <Collaborator name="Fernando" foto="foto" cargo="Frontend" />
+            <Collaborator name="Paola" foto="foto" cargo="Backend" />
+            <Collaborator name="Diego" foto="foto" cargo="Diseñador UI" />
+            <Collaborator name="Sofia" foto="foto" cargo="Diseñador UX" />
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default Collaborators;
