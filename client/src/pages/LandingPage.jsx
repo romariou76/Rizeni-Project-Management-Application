98dab20f03cbf94ecb407fe348111e6ab2b260ed
romariou76 <illeniumr@gmail.com>
2022-11-15 21:11:49 -0500

    feat: rediseñe la landign page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -11,7 +11,7 @@ function LandingPage() {
       <div className="hero">
         <div className="hero-info">
           <p>Bienvenido a Rizeni <br></br>
-          <p>100% CSS Pure</p>Gestiona el desarrollo de tus proyectos con una interfaz sencilla y agradable</p>
+          Gestiona el desarrollo de tus proyectos con una interfaz sencilla y agradable</p>
           <w>Rizeni para la Corporación Yupanna</w>
           <br></br><br></br><br></br><br></br>
           <Link className="login-button" to="/register">Registrate ahora!</Link>
@@ -21,12 +21,7 @@ function LandingPage() {
         </div>
 
       </div>
-      <svg className="svg" viewBox="0 0 500 150" preserveAspectRatio="none">
-          <path
-            className="path"
-            d="M-8.74,99.19 C264.95,64.64 256.49,237.34 527.93,54.78 L544.86,213.66 L-35.83,169.25 Z"
-          ></path>
-        </svg>
+     
     </div>
   );
 }
