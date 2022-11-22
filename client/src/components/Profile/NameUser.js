df22311fb0b1e888db1165f953b1cc969eded8cb
romariou76 <illeniumr@gmail.com>
2022-11-22 16:42:37 -0500

    feat: Agregue campos estaticos al perfil

--- client/src/components/Profile/NameUser.js
+++ client/src/components/Profile/NameUser.js
@@ -3,7 +3,7 @@ function NameUser() {
     <div className="flex justify-center">
       <div className="flex-col">
         <p className="text-black text-2xl text-center font-bold py-2">Eva Sofia</p>
-        <p className="text-xs text-center">UX Designer</p>
+        <p className="text-xs text-center text-">UX Designer</p>
       </div>
     </div>
    );
--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -7,6 +7,7 @@ import Navbar from '../../app/components/Navbar/Navbar';
 import CardStat from "./CardStat.js";
 import Chart from "./charts/Chart"
 import AreaChart from "./charts/AreaChart";
+import Actividad from "./Actividad.js";
 
 function Profile(props) {
   return (
@@ -14,21 +15,26 @@ function Profile(props) {
       <Navbar />
       <div className="Profile">
         <div className="profile-info">
-          <p className="text-black font-bold text-lg">Vision General</p>
 
-          <overview className="overview pr-16 mt-2">
-            <div className="flex justify-start gap-x-2 desktop:justify-start desktop:gap-16">
-              <CardStat />
+          <p className="text-black font-bold text-lg">Vision General</p>
+            <overview className="overview pr-16 mt-2">
+              <div className="flex justify-start gap-x-2 desktop:justify-start desktop:gap-16">
+                <CardStat />
+              </div>
+            </overview>
+
+            <p className="text-black font-bold text-lg">Densidad de trabajo</p>
+            <div className="dendem flex mt-2">
+              <div className="density rounded-lg w-96">
+                <AreaChart />
+              </div>
+              <demography className="demography rounded-lg">
+                <Chart />
+              </demography>
             </div>
-          </overview>
-        <p className="text-black font-bold text-lg">Densidad de trabajo</p>
-          <div className="dendem flex mt-2">
-            <div className="density rounded-lg w-96">
-               <AreaChart />
-            </div>
-            <demography className="demography rounded-lg">
-               <Chart />
-            </demography>
+
+          <div className="activity-user">
+            <Actividad />
           </div>
 
           {/* <div className="active-users"></div> */}
@@ -40,6 +46,22 @@ function Profile(props) {
           </div>
           <NameUser />
           <Stat />
+          <p className="text-black text-lg ml-5 my-2 font-bold">Detalles</p>
+          <div className="px-5 flex flex-col gap-y-2">
+            <p className="text-black">Username: <span className="opacity-40">Eva Sofia</span></p>
+            <p className="text-black">Correo: <span className="opacity-40">evita@gmail.com</span></p>
+            <p className="text-black">Estado: <span className="opacity-40">Activo</span></p>
+            <p className="text-black">Rol: <span className="opacity-40">UX Design</span></p>
+            <p className="text-black">Tax ID: <span className="opacity-40">Tax-9151</span></p>
+            <p className="text-black">Contacto: <span className="opacity-40">(+51) 900151546</span></p>
+            <p className="text-black">Lenguaje: <span className="opacity-40">Español</span></p>
+            <p className="text-black">Nacionalidad: <span className="opacity-40">Peruano</span></p>
+            <div className="flex justify-start gap-x-8 mt-4">
+              <button className="btn btn-active btn-info text-white px-10">Edit</button>
+              <button className="btn btn-outline btn-error">Suspended</button>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
