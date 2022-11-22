1880668f448c0a34f4386a597ea229d4dfb5d18e
romariou76 <illeniumr@gmail.com>
2022-11-22 18:56:47 -0500

    feat: Agregue diseño al componente lista de tareas

---
+++ client/src/components/Tasks/Table.js
@@ -0,0 +1,43 @@
+import "./Table.css"
+
+function Table() {
+  return ( 
+    <>
+      <div className="overflow-x-auto">
+  <table className="table w-full">
+
+
+    <thead>
+      <tr className="border-th">
+        <th className="bg-white">#</th>
+        <th className="bg-white">Titulo</th>
+        <th className="bg-white">Etiqueta</th>
+        <th className="bg-white">Estado</th>
+        <th className="bg-white">Fecha</th>
+        <th className="bg-white">Foto</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* <!-- row 1 --> */}
+      <tr className="border-th">
+        <th className="bg-white">1</th>
+        <td className="bg-white">Realizar la Navbar</td>
+        <td className="bg-white text-primary">Facil</td>
+        <td className="bg-white text-warning">En Progreso</td>
+        <td className="bg-white">Nov. 17</td>
+        <td className="bg-white">
+          <div className="mask mask-squircle w-10 h-10">
+            <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" alt="Avatar Tailwind CSS Component" />
+          </div>
+        </td>
+      </tr>
+      {/* <!-- row 2 --> */}
+
+    </tbody>
+  </table>
+</div>
+    </>
+   );
+}
+
+export default Table;
--- client/src/components/Tasks/Tasks.js
+++ client/src/components/Tasks/Tasks.js
@@ -1,8 +1,45 @@
-function Tasks() {
-  return ( 
-    <div>TASKP API</div>
+import "./Task.css"
+import React from 'react';
+import Navbar from '../../app/components/Navbar/Navbar';
+import Table from "./Table";
 
-   );
+function Tasks() {
+  return (
+    <div className='w-screen Task-page flex-col'>
+      <Navbar />
+      <div className="flex m-4 tasks-container">
+        <div className="drawer-task">
+          <button className="btn btn-primary m-4 px-14">Agregar Tarea</button>
+          <ul className="ml-2">
+            <li><a className="text-lg text-gray-500 hover:text-primary">My Tasks</a></li>
+            <li><a className="text-lg text-gray-500 hover:text-primary">Importante</a></li>
+            <li><a className="text-lg text-gray-500 hover:text-primary">Completed</a></li>
+          </ul>
+          <br></br>
+          <div className="flex justify-between">
+            <p className="mx-4 mb-2">TAGS</p>
+            <span className="mr-8">+</span>
+          </div>
+          <ul className="ml-2">
+            <li className="text-lg text-gray-500 hover:text-primary">Team</li>
+            <li className="text-lg text-gray-500 hover:text-primary">Low</li>
+            <li className="text-lg text-gray-500 hover:text-primary">Medium</li>
+            <li className="text-lg text-gray-500 hover:text-primary">High</li>
+            <li className="text-lg text-gray-500 hover:text-primary">Update</li>
+          </ul>
+        </div>
+        <div className="drawer-content flex-col">
+          <div className="form-control">
+            <input type="text" placeholder="Search" className="input input-bordered" />
+          </div>
+
+          <Table />
+
+        </div>
+      </div>
+    </div>
+
+  );
 }
 
 export default Tasks;
