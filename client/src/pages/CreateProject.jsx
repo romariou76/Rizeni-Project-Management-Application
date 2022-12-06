0c8ac5e949d263ed69ecb8892589820e3e6823ef
romariou76 <illeniumr@gmail.com>
2022-12-05 21:12:47 -0500

    feat: Agregue la funcionalidad de ahora si poder crear un proyecto en el cual podamos ingresar

--- client/src/pages/CreateProject.jsx
+++ client/src/pages/CreateProject.jsx
@@ -1,37 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "../styles/WorkSpace.css"
 import TextField from '@mui/material/TextField';
 import Herowork from "../images/hero-work.png"
 import { Link } from 'react-router-dom';
 
-function CreateProject() {
+function CreateProject({addProyecto}) {
 
-  // Codigo para el guardado en LocalStorage
-  const [inputText, setinputText] = useState("");
 
-  const handleInputChange = (e) => {
-    const text = e.target.value
-    setinputText(text);
-    console.log(inputText);
+
+
+  const [projectData, setProjectData] = useState({
+    nombre: '',
+    tipo: '',
+    descripcion: '',
+    id: null
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (projectData.nombre !== '' && projectData.descripcion !== '' ){
+
+    projectData.id = Math.floor(Math.random() * 100)
+    addProyecto(projectData)
+    setProjectData({
+      nombre: '',
+      tipo: '',
+      descripcion: '',
+      id: null
+    })
+  } else {
+    alert("Por favor agregue un nombre y descripcion")
   }
+}
 
-  const saveData = () => {
-    localStorage.setItem('nombre', inputText);
+  const handleChange = (e) => {
+    setProjectData({
+      ...projectData,
+      [e.target.name]: e.target.value
+    })    
   }
-  // ----------------------------
+  
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
+
+  <form onSubmit={handleSubmit}>
     <div className='WorkSpace'>
       <div className='form-modal'>
         <p className='text-lg text-black font-bold mb-4'>Vamos a crear un Proyecto<br></br></p>
         <p className='mb-4 text-slate-500'>Impulse su productividad facilitandoles a todos <br></br> el acceso al flujo de trabajo en una unica ubicacion </p>
         <br></br>
         <p className='title-space-style mb-1 text-black'>Nombre del nuevo Proyecto</p>
-        <TextField onChange={handleInputChange} className='TextField' id="outlined-basic" label="" placeholder='Ingrese un nombre' variant="outlined" />
+        <TextField className='TextField' name='nombre' placeholder='Ingrese un nombre' onChange={handleChange} value={projectData.nombre}/>
         <p className='title-single-style mt-2 text-slate-500'>Este es el nombre de su empresa, equipo o organizacion</p>
         <br></br>
         <p className='font-bold mb-2 text-black'>Tipo de Proyecto</p>
-        <select className='select-project text-slate-500'>
+        <select className='select-project text-slate-500' name="tipo" onChange={handleChange}>
           <option>Marketing</option>
           <option>Educacion</option>
           <option>Facturacion</option>
@@ -43,10 +70,11 @@ function CreateProject() {
         <p className='title-single-style mt-2 text-slate-500'>El tipo de proyecto ayudara en el desarrollo del proyecto</p>
         <br></br>
         <p className='title-description-style mb-2 text-black'>Descripcion del Proyecto</p>
-        <textarea className='text-area-space text-slate-500'></textarea>
+        <textarea className='text-area-space text-slate-500' name="descripcion" onChange={handleChange} value={projectData.descripcion}></textarea>
         <br></br>
         {/* <p className='title-single-style'>Incorpora a los miembros con unas cuantas palabras sobre tu espacio <br></br> de trabajo</p> */}
-        <Link onClick={saveData} to="/menu/new" className='continue-bottom'>Continuar</Link>
+        {/* <Link to="/menu/new" className='continue-bottom'>Continuar</Link> */}
+        <input type="submit" value="Agregar" onClose={handleClose}/>
       </div>
       {/* <div>
       <svg className='hero-wave' width="100%" height="100%" id="svg" viewBox="0 0 1440 600" xmlns="http://www.w3.org/2000/svg" class="transition duration-300 ease-in-out delay-150"><path d="M 0,600 C 0,600 0,300 0,300 C 115.08133971291863,348.4593301435407 230.16267942583727,396.91866028708137 318,406 C 405.83732057416273,415.08133971291863 466.43062200956945,384.78468899521533 541,335 C 615.5693779904306,285.21531100478467 704.1148325358851,215.9425837320574 821,224 C 937.8851674641149,232.0574162679426 1083.1100478468902,317.44497607655506 1191,343 C 1298.8899521531098,368.55502392344494 1369.444976076555,334.27751196172244 1440,300 C 1440,300 1440,600 1440,600 Z" stroke="none" stroke-width="0" fill="#9900ef" fill-opacity="1" class="transition-all duration-300 ease-in-out delay-150 path-0" transform="rotate(270 940 80)"></path></svg>
@@ -55,6 +83,7 @@ function CreateProject() {
         <img className='hero-img-work' alt='hero-work' src={Herowork}></img>
       </div>
     </div>
+  </form>
   );
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,29 +6,41 @@ import "../styles/NavbarMain.css"
 import "../styles/Home.css"
 import { Link } from 'react-router-dom';
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  boxShadow: 20,
-};
+const style = { position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', boxShadow: 20, };
 
-export default function BasicModal() {
+// const baseDatos = [
+//   { id: 1, nombre: 'Brenda', descripcion: 'Milene' },
+//   { id: 2, nombre: 'Barcelona1', descripcion: 'españa' },
+//   { id: 3, nombre: 'Barcelona2', descripcion: 'españita' },
+// ]
+
+function BasicModal() {
+
+  // Mostrar y guardar datos de los proyectos
+  const [proyectos, setProyectos] = useState(() => {
+    const saveProyectos = window.localStorage.getItem("proyectosData");
+    if (saveProyectos) {
+      return JSON.parse(saveProyectos);
+    } else {
+      return []
+    }
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem("proyectosData", JSON.stringify(proyectos))
+  }, [proyectos])
+
+  // Agregar Proyecto
+  const addProyecto = (proyecto) => {
+    setProyectos([
+      ...proyectos,
+      proyecto
+    ])
+  }
+
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-
-  // Retornando datos del proyecto
-  const [nombre, setNombre] = useState('');
-
-  const getData = () => {
-    return localStorage.getItem('nombre');
-  }
-
-  useEffect(() => {
-    setNombre(getData());
-  }, []);
 
   return (
     <div>
@@ -37,17 +49,15 @@ export default function BasicModal() {
           <li><a href="default.asp">RIZENI</a></li>
           <li><a href="#new">Tu trabajo</a></li>
           <li><select className="select-navbar">
-              <option value="0">Proyectos</option>
-              <option value="0">Rizeni</option>
-              <option value="1">PeepoApp</option>
-              <option value="2">Optical UI</option>
-              <option value="3">PeepoPay</option>
+            <option value="0">Proyectos</option>
+            <option value="0">Rizeni</option>
+            <option value="1">PeepoApp</option>
+            <option value="2">Optical UI</option>
+            <option value="3">PeepoPay</option>
           </select></li>
-          <li><a href="about.asp">Filtros</a></li>
-          <li><a href="about.asp">Paneles</a></li>
-          <li><a href="about.asp">Personas</a></li>
+          <li><a href="about.asp">Usuarios</a></li>
           <li><a href="/">Salir</a></li>
-          <button onClick={handleOpen} className="button-modal">Crear</button>
+          <button onClick={handleOpen} className="button-modal">Crear Proyecto</button>
         </div>
 
         <div className="content-navbar">
@@ -67,7 +77,7 @@ export default function BasicModal() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <CreateProject />
+          <CreateProject addProyecto={addProyecto}/>
         </Box>
       </Modal>
 
@@ -77,24 +87,25 @@ export default function BasicModal() {
         <h3 className='text-xl text-black mb-12'>Tus proyectos recientes</h3>
         <div className='flex flex-row'>
 
-        <div className='card bg-white p-4 w-96 mr-12'>
-          <p className='text-black my-2 font-bold'>{nombre}</p>
-          <p className='my-2 text-slate-500'>Este proyecto esta hecho para desarrollar el siguiente proyecto para la empresa Gloria
-            dicha empresa esta sobrevalorada
-          </p>
-          <p className='mb-4'>Ultima vez visto: hace 4 dias</p>
-          <Link to="/menu/profile" className="project-name-link">Ir a: {nombre}</Link>
-        </div>
-        <div className='card bg-white p-4 w-96'>
-          <p>{nombre}</p>
-          <p>Este proyecto esta hecho para desarrollar el siguiente proyecto para la empresa Gloria
-            dicha empresa esta sobrevalorada
-          </p>
-          <Link to="/menu/profile" className="project-name-link">Ir a: {nombre}</Link>
-        </div>
+          {
+            proyectos.length === 0 ? <p className='text-black'>Aun no hay proyectos</p>
+              : proyectos.map((proyecto, index) => {
+                return <div className='card bg-white p-4 w-96 mr-12'>
+                  <p className='text-black my-2 font-bold'>{proyecto.nombre}</p>
+                  <p className='my-2 text-slate-500'>
+                    {proyecto.descripcion}
+                  </p>
+                  <p>Tipo: {proyecto.tipo}</p>
+                  <p className='mb-4'>Ultima vez visto: hace 4 dias</p>
+                  <Link to="/menu/profile" className="project-name-link">Ir a: {proyecto.nombre}</Link>
+                </div>
+              })
+          }
 
         </div>
       </div>
     </div>
   );
 }
+
+export default BasicModal;
